cceb3d59b99284242a454eebfd219df5d1548a6b
Briain <Briain@BRIAIN-PC>
2016-12-04 19:44:17 +0000

    Add project files.

---
+++ Bites3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bites3.MvcApplication" Language="C#" %>
---
+++ Bites3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bites3.Startup))]
+namespace Bites3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
